e3f4833d3003b8c0d82bab97e2dac9ac9c2117c8
Elliott Alexander <escottalexander@gmail.com>
2018-07-14 15:01:47 -0400

    added new example to sort function

--- sort.js
+++ sort.js
@@ -13,4 +13,16 @@ function sortArr(arr) {
 
 sortArr(["a", "g", "b", "f", "c", "e", "d"])
 
-sortArr([1, 2, 3, 4, 5, 6, 7, 20, 30, 40, 8, 9])
+sortArr([1, 2, 3, 4, 5, 6, 7, 20, 30, 40, 8, 9, 22, 27, 400])
+
+sortArr([1, 2, 3, 4, 5, 6, 7, 8, -1, -2, -3, -4, -5, -6, -7, -8])
+
+
+// Notice how this abbreviated version doesn't yeild a correct result when used on strings. a - b returns NaN. However, this version works perfectly with numbers.
+function sortArr2(arr) {
+    console.table(arr.sort((a, b) => a - b)
+    );
+}
+
+sortArr2(["a", "g", "b", "f", "c", "e", "d"])
+
